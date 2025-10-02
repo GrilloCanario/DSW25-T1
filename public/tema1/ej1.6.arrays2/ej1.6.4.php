9656f2f7901e0fad8ccfc391a83f5b9a772427cd
PedroBarrera <pedrodbarrerafdez@gmail.com>
2025-10-02 12:59:00 +0100

    1.6.4 house

--- public/tema1/ej1.6.arrays2/ej1.6.4.php
+++ public/tema1/ej1.6.arrays2/ej1.6.4.php
@@ -7,7 +7,16 @@
 </head>
 <body>
     <?php
-    
+    $numerosMultidimensional = [];
+    $count = 1;
+    echo "<table><tr><caption><strong>Tabla HTML:</strong></caption</tr></tbody>";
+    for($i = 0; $i < 16 ; $i++){
+        for($j = 0; $j < 10; $j++){
+            print_r("<pre>" . $numerosMultidimensional[$i][$j] = $count . "</pre>");
+            $count++;
+        }
+    }
+    echo "</tbody></table>";
     ?>
 </body>
 </html>
